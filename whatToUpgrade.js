6f7b1d162fc8912e2883093e41a8972d5d0dc5e2
Johi <155817168+fohij@users.noreply.github.com>
2024-01-29 17:14:58 +0100

    .

--- whatToUpgrade.js
+++ whatToUpgrade.js
@@ -26,21 +26,37 @@ There are also different Makers which will be added later
 
 */
 
-
+const start = ["Your next Upgrade should be: ", "You should really work on: ", "Maybe getting "];
 const bikeUpgrades = ["Frame", "Brakes", "Groupset", "Handlebars", "Wheels", "Pedals"];
 const fitnessUpgrades = ["Base Fitness", "VO2 Max", "just shave your legs", "Upper Body Strength", "Lower Body Strength", "Just go eating"];
 const gearUpgrades = ["new Jersey", "new Bib", "new Shoes", "new Glasses", "new Jacket"];
 
+    let antwort = [];
+
+    let randomstart=(Math.floor(Math.random() * start.length));
+    let randBikeUpgrade = Math.floor(Math.random()*bikeUpgrades.length);
+    let randFitnessUpgrade = Math.floor(Math.random()*fitnessUpgrades.length);
+    let randGearUpgrade = Math.floor(Math.random()*gearUpgrades.length);
+
+    antwort.push(start[randomstart])
+
+    switch (randomstart){
+        case 0:
+            antwort.push(bikeUpgrades[randBikeUpgrade]);
+            break;
+        case 1:
+            antwort.push(fitnessUpgrades[randFitnessUpgrade]);
+            break;
+        
+        case 2:
+            antwort.push(gearUpgrades[randGearUpgrade]);
+            break;
+    }
+   
+console.log(`${antwort[0]} ${antwort[1]}`);
 
 
-// for (element in bikeUpgrades){
-//     console.log(bikeUpgrades[element])
-// }
 
-console.log(bikeUpgrades[Math.floor(Math.random(bikeUpgrades.length)*6)]);
-function assambler (){
-    let antwort ="Your next upgrade should be: " + bikeUpgrades[Math.random(bikeUpgrades.length*6)];
 
-}
 
 
